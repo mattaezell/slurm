ea2a0568c247f9cb12490c9118a8c4aacd5f6d65
Danny Auble <da@schedmd.com>
2018-05-25 12:04:00 -0600

    Make job_array_port_sched return the new job_ptr it potentially makes.
    This will be needed in a future commit.

--- src/slurmctld/job_mgr.c
+++ src/slurmctld/job_mgr.c
@@ -18410,13 +18410,13 @@ extern void job_array_pre_sched(struct job_record *job_ptr)
 }
 
 /* If this is a job array meta-job, clean up after scheduling attempt */
-extern void job_array_post_sched(struct job_record *job_ptr)
+extern struct job_record *job_array_post_sched(struct job_record *job_ptr)
 {
-	struct job_record *new_job_ptr;
+	struct job_record *new_job_ptr = NULL;
 	char jobid_buf[32];
 
 	if (!job_ptr->array_recs || !job_ptr->array_recs->task_id_bitmap)
-		return;
+		return job_ptr;
 
 	if (job_ptr->array_recs->task_cnt <= 1) {
 		/* Preserve array_recs for min/max exit codes for job array */
@@ -18448,6 +18448,7 @@ extern void job_array_post_sched(struct job_record *job_ptr)
 					job_ptr->array_task_id)) {
 			_add_job_array_hash(job_ptr);
 		}
+		new_job_ptr = job_ptr;
 	} else {
 		new_job_ptr = job_array_split(job_ptr);
 		if (new_job_ptr) {
@@ -18460,6 +18461,8 @@ extern void job_array_post_sched(struct job_record *job_ptr)
 			      jobid2fmt(job_ptr, jobid_buf, sizeof(jobid_buf)));
 		}
 	}
+
+	return new_job_ptr;
 }
 
 /* _kill_dependent()
